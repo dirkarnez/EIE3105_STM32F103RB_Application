7d54f19b6aac7b7f56fb89519de265b390c74306
Dirk Arnez <smalldirkalex@gmail.com>
2024-01-04 20:31:03 +0800

    - update local-build.cmd

--- local-build.cmd
+++ local-build.cmd
@@ -1,3 +1,3 @@
 @echo off
-"%USERPROFILE%\Downloads\keil-mdk-v5.38a-portable\UV4.Portable.exe" --project=EIE3105_STM32F103RB_Application.uvprojx &&^
+"%USERPROFILE%\Downloads\Keil_v5.38a_with_packages\UV4.Portable.exe" --project=EIE3105_STM32F103RB_Application.uvprojx &&^
 pause
